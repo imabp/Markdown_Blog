10ec0073d5ed5d79634d864db72ae33ff78b8038
imabp <abir.pal899@gmail.com>
2020-03-16 19:05:33 +0530

    Made it more good

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 const express = require('express')
 const mongoose = require('mongoose')
 const Article = require('./models/article')
@@ -5,12 +8,12 @@ const articleRouter = require('./routes/articles')
 const methodOverride = require('method-override')
 const app = express()
 
-mongoose.connect('mongodb://localhost/blog', {
+mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
 })
 
 app.set('view engine', 'ejs')
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({limit: '10mb', extended: false }))
 app.use(methodOverride('_method'))
 
 app.get('/', async (req, res) => {
@@ -20,4 +23,4 @@ app.get('/', async (req, res) => {
 
 app.use('/articles', articleRouter)
 
-app.listen(5000)
+app.listen(process.env.PORT || 5000);
